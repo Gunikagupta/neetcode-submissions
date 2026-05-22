05c273abcdf3efaab908022e6ff056ff8a2ac0ef
Gunika Gupta <niagupta84@gmail.com>
2026-05-22 14:59:15 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        int maxsum=INT_MIN;
+
+        for(int i=0;i<n;i++)
+        {
+            
+            
+            sum+=nums[i];
+            maxsum=max(maxsum,sum);
+            
+            if(sum<0)
+            sum=0;
+            
+
+           
+
+
+        
+           
+
+
+        }
+        
+
+
+        return maxsum;
+
+    }
+};
